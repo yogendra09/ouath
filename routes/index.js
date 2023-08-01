ea182c49d1f3a635aca1297b38b05dd4d819816b
yogendra09 <yogendrayadav168@gmail.com>
2023-08-02 02:41:46 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
   clientID: process.env['GOOGLE_CLIENT_ID'],
   clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-  callbackURL: '/oauth2/redirect/google',
+  callbackURL: 'https://oauth-ayez.onrender.com/oauth2/redirect/google',
   scope: [ 'email','profile' ]
 },async function verify(issuer, profile, cb) {
   try{
